19e71b599381bafdfc60d6daeeec093c04a7a8e1
u1374012 <u1374012@utah.edu>
2023-02-04 12:59:33 -0700

    Updated. https://github.com/UofUEpiBio/PHS7045-advanced-programming/issues/7

--- week-02/week_2_lab.R
+++ week-02/week_2_lab.R
@@ -1,14 +1,15 @@
 
+# Note: Control arm is indexed 1 and treatment arms are indexed 2, 3 and 4.
+
 ##### ----------- Design 1 -----------#####
 
-# Generating data.
-
-fun_1 <- function(k=1000, N = 228, seed = 100, alpha = 0.35, beta=0.65, p=0.35, delta=0.9912){
+design1_func <- function(k=1000, N=228, seed=100, alpha=0.35, beta=0.65,
+                         p=0.35, delta=0.9912){
   set.seed(seed)
   y_arm0 <- rbinom(N/4, 1, p=p)
   y_arm1 <- rbinom(N/4, 1, p=p)
   y_arm2 <- rbinom(N/4, 1, p=p)
-  y_arm3 <- rbinom(N/4, 1, p=p) # some changes..
+  y_arm3 <- rbinom(N/4, 1, p=p)
 
   data_mat <- as.matrix(cbind(y_arm0, y_arm1, y_arm2, y_arm3))
   # Drawing from posterior distribution.
@@ -18,67 +19,14 @@ fun_1 <- function(k=1000, N = 228, seed = 100, alpha = 0.35, beta=0.65, p=0.35, 
   p_arm3 <- rbeta(k, alpha + sum(y_arm3), beta + N/4 - sum(y_arm3))
   p_vec <- c(sum(as.numeric(p_arm1>p_arm0))/k, sum(as.numeric(p_arm2>p_arm0))/k, 
            sum(as.numeric(p_arm3>p_arm0))/k)
-  return(c(which.max(p_vec),p_vec))
+  return(c(which.max(p_vec)+1,p_vec))## +1 because indexing of arms starts with 1.
 }
 
 ##### ----------- Design 2 -----------#####
 
-# Patients 1-40
+# Non-modular version of design 2.
 
-k=1000
 set.seed(100)
-
-n01 <- 10
-n11 <- 10
-n21 <- 10
-n31 <- 10
-
-y_arm01 <- rbinom(n01, 1, p=p)
-y_arm11 <- rbinom(n11, 1, p=p)
-y_arm21 <- rbinom(n21, 1, p=p)
-y_arm31 <- rbinom(n31, 1, p=p)
-
-p_arm01 <- rbeta(k, alpha + sum(y_arm01), beta + n01 - sum(y_arm01))
-p_arm11 <- rbeta(k, alpha + sum(y_arm11), beta + n11 - sum(y_arm11))
-p_arm21 <- rbeta(k, alpha + sum(y_arm21), beta + n21 - sum(y_arm21))
-p_arm31 <- rbeta(k, alpha + sum(y_arm31), beta + n31 - sum(y_arm31))
-
-d <- as.data.frame(cbind(p_arm01, p_arm11, p_arm21, p_arm31))
-
-d$max_index <- apply(d, 1, FUN = function(x) which.max(x))
-
-#v01 <- sum(as.numeric(d$max_index==1))/k
-v11 <- sum(as.numeric(d$max_index==2))/k
-v21 <- sum(as.numeric(d$max_index==3))/k
-v31 <- sum(as.numeric(d$max_index==4))/k
-
-v01 <- min(v11*((n11+1)/(n01+1)) + v21*((n21+1)/(n01+1)) + v31*((n31+1)/(n01+1)),
-           max(v11, v21, v31))
-
-v01 <- v01/(v01+v11+v21+v31)
-v11 <- v11/(v01+v11+v21+v31)
-v21 <- v21/(v01+v11+v21+v31)
-v31 <- v31/(v01+v11+v21+v31)
-
-# Patients 41-80 
-
-n02 <- round(v01*40,0)
-n12 <- round(v11*40,0)
-n22 <- round(v21*40,0)
-n32 <- round(v31*40,0)
-
-y_arm02 <- rbinom(n02, 1, p=p)
-y_arm12 <- rbinom(n12, 1, p=p)
-y_arm22 <- rbinom(n22, 1, p=p)
-y_arm32 <- rbinom(n32, 1, p=p)
-
-p_arm02 <- rbeta(k, alpha + sum(y_arm01), beta + n02 - sum(y_arm01))
-p_arm12 <- rbeta(k, alpha + sum(y_arm11), beta + n12 - sum(y_arm11))
-p_arm22 <- rbeta(k, alpha + sum(y_arm21), beta + n22 - sum(y_arm21))
-p_arm32 <- rbeta(k, alpha + sum(y_arm31), beta + n32 - sum(y_arm31))
-
-#----#
-
 total_patients <- 228
 k=1000
 n_allocation <- c(0,0,0,0)
@@ -140,7 +88,7 @@ while (patients_assigned < total_patients){
   
 }
 
-# Modularised version
+# Modular version of design 2.
 
 n_allocation_func <- function(n, arm_weights_vec){
   n_allocation_vector <- numeric()
@@ -218,35 +166,30 @@ best_arm <- function(p_arm_inputlist){
   p_vec <- c(sum(as.numeric(p_arm_inputlist[[2]]>p_arm_inputlist[[1]]))/len,
              sum(as.numeric(p_arm_inputlist[[3]]>p_arm_inputlist[[1]]))/len, 
              sum(as.numeric(p_arm_inputlist[[4]]>p_arm_inputlist[[1]]))/len)
-  return(c(which.max(p_vec)+1,p_vec)) ## +1 becuase my indexing of arms starts with 1.
+  return(c(which.max(p_vec)+1,p_vec)) ## +1 because indexing of arms starts with 1.
 }
 
-keep_y_arm_list <- list(numeric(0), numeric(0), numeric(0), numeric(0))
-keep_n_allocation_vector <- c(0,0,0,0)
-total_patients <- 228
-k=1000
-n_allocation <- c(0,0,0,0)
-arm_weights <- c(0.25, 0.25, 0.25, 0.25)
-patients_num <- 40
-p <- 0.35
-alpha <- 0.35
-beta <- 0.65
-patients_assigned <- 0
-
-while (patients_assigned < total_patients){
+design2_func <- function(seed=100, total_patients=228, k=1000, patients_num=40, p=0.35,
+                         alpha=0.35, beta=0.65, delta=0.9892){
+  set.seed(seed)
+  keep_y_arm_list <- list(numeric(0), numeric(0), numeric(0), numeric(0))
+  keep_n_allocation_vector <- c(0,0,0,0)
+  n_allocation <- c(0,0,0,0)
+  arm_weights <- c(0.25, 0.25, 0.25, 0.25)
+  patients_assigned <- 0
   
-  interim <- min(patients_num, total_patients - patients_assigned)
-
-  n_allocation <- n_allocation_func(interim, arm_weights)
-  keep_n_allocation_vector <- storing_n_allocation_func(keep_n_allocation_vector,
+  while (patients_assigned < total_patients){
+    interim <- min(patients_num, total_patients - patients_assigned)
+    
+    n_allocation <- n_allocation_func(interim, arm_weights)
+    keep_n_allocation_vector <- storing_n_allocation_func(keep_n_allocation_vector,
                                                         n_allocation)
-  y_arm_list <- binom_outcome_sample_func(n_allocation, p)
-  keep_y_arm_list <- storing_outcome_sample_func(keep_y_arm_list, y_arm_list)
-  posterior <- posterior_func(k, alpha, beta, keep_y_arm_list,
+    y_arm_list <- binom_outcome_sample_func(n_allocation, p)
+    keep_y_arm_list <- storing_outcome_sample_func(keep_y_arm_list, y_arm_list)
+    posterior <- posterior_func(k, alpha, beta, keep_y_arm_list,
                               keep_n_allocation_vector)
-  arm_weights <- rar_weights_update_func(posterior, n_allocation)
-  
-  patients_assigned <- patients_assigned + interim
-}
-
-best_arm(posterior)
+    arm_weights <- rar_weights_update_func(posterior, n_allocation)
+    patients_assigned <- patients_assigned + interim
+    }
+  return(best_arm(posterior))
+  }
