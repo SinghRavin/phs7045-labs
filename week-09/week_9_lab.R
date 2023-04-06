9bff2583a0e2226d263d71175933a825fa55e96f
u1374012 <u1374012@utah.edu>
2023-04-06 17:12:59 -0600

    Added netplot logfile.

---
+++ week-09/week_9_lab.R
@@ -0,0 +1,41 @@
+library(netplot)
+library(igraph)
+library(igraphdata)
+
+data(yeast)
+
+set.seed(889)
+
+# Netplot objects are grobs (like ggplot2)
+np <- nplot(
+  yeast,
+  # Too many vertices, so just a sample
+  sample.edges = .1 
+)
+
+# So we need to print them
+print(np)
+
+col <- V(UKfaculty)$Group
+
+data("UKfaculty")
+plot <- nplot(UKfaculty, vertex.color=col)
+
+png("plot.fig", width=1024, height=780)
+
+set.seed(1231)
+ans <- nplot(
+  UKfaculty,
+  vertex.color = col,
+  edge.line.breaks = 20,
+  # edge.curvature = pi/3 * 4,
+  bg.col = "black"
+)
+
+png("netplotfig.png", width = 1024, height = 780)
+print(ans)
+dev.off()
+
+saveRDS(ans, file="netplotfig.rds")
+
+
